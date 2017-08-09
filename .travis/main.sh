78b18b1153c813026d4bb662b1cd7266145aecc0
Ivan Santos <pragmaticivan@gmail.com>
2017-08-09 10:29:58 -0700

    Adds travis script

---
+++ .travis/main.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -o errexit
+set -o xtrace
+
+main() {
+  case $1 in
+    setup-machine)
+      setup_machine
+      ;;
+
+    install-packages)
+      install_packages
+      ;;
+
+    deploy)
+      deploy
+      ;;
+
+    *)
+      echo "Unknown command '$1'"
+      ;;
+
+  esac
+}
+
+setup_machine() {
+  echo "Installing WeDeploy Command-line"
+  curl https://cdn.wedeploy.com/cli/latest/wedeploy.sh -sL | bash
+}
+
+install_packages() {
+  cd api && npm install -g npm@latest && npm install
+}
+
+deploy() {
+  echo "Deploying to wedeploy"
+}
+
+main "$@"
